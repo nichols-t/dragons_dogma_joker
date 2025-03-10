a3bc76f182831e10dae170647fbdb39f5759af47
nichols-t <tynichols0@gmail.com>
2025-03-10 17:42:06 -0400

    No longer stores actual destroyed card table, but rather recreates it (fixes Ankh crash). Now juices itself when eating hearts.

--- DragonsDogma.lua
+++ DragonsDogma.lua
@@ -30,7 +30,6 @@ SMODS.Joker {
   atlas = "DragonsDogma",
   pos = { x= 0, y = 0 },
   cost = 10,
-  discovered = true,
   unlocked = true,
   -- Card config (stores cards)
   config = {
@@ -38,39 +37,40 @@ SMODS.Joker {
       stored_cards = {}
     }
   },
-  -- todo crashes when ankhed if it has cards copied. Need to either:
-  -- o prevent Ankh altogether (not sure how)
-  -- o copy all stored cards so that each copy has its own set
-  -- o copy but do not transfer the stored cards
   -- When removed from deck, return destroyed hearts
   remove_from_deck = function(self, card, from_debuff)
-    -- todo any other states that must be excluded?
     -- not: not state 4 (GAME_OVER) to prevent this causing a crash on exit after failed run
     if not from_debuff and G.STATE ~= 4 then
+      -- determine where to put the cards that are being added back
+      local _area = G.deck
+      if G.STATE < 4 or G.STATE == 19 then
+        _area = G.hand
+      end
       for _, v in ipairs(card.ability.extra.stored_cards) do
-        -- Copied from DNA
-        G.playing_card = (G.playing_card and G.playing_card + 1) or 1
-        local _card = copy_card(v, nil, nil, G.playing_card)
-        _card:set_seal('Red', true)
-        _card:add_to_deck()
-        G.deck.config.card_limit = G.deck.config.card_limit + 1
-        table.insert(G.playing_cards, _card)
-        -- todo this should probably be tested more extensively to make sure it works:
-        -- should add to hand if we are currently in a blind
-        -- add to deck otherwise
-        if G.STATE < 4 or G.STATE == 19 then
-          G.hand:emplace(_card)
-        else
-          G.deck:emplace(_card)
+        -- doesn't seem that there's a better way to get this string
+        local _rank = v.id
+        if _rank == 14 then
+          _rank = 'A'
+        elseif _rank == 13 then
+          _rank = 'K'
+        elseif _rank == 12 then
+          _rank = 'Q'
+        elseif _rank == 11 then
+          _rank = 'J'
+        elseif _rank == 10 then
+          _rank = 'T'
         end
-        _card.states.visible = nil
 
-        G.E_MANAGER:add_event(Event({
-          func = function()
-              _card:start_materialize()
-              return true
-          end
-        })) 
+        -- I have no idea what this line is really for
+        G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+        -- create a card that should be equivalent to the one that was stored
+        local card_init = {
+          front = G.P_CARDS['H'..'_'.._rank]
+        }
+        local _card = create_playing_card(card_init, _area, nil, nil, {G.C.SET.Default})
+        _card:set_seal('Red', true)
+        _card:set_edition(v.edition, nil, nil)
+        _card.ability.perma_bonus = v.perma_bonus
     end
   end
 end,
@@ -78,7 +78,21 @@ end,
   calculate = function(self, card, context)
     if context.destroy_card and context.cardarea == G.play and not context.blueprint then
       if context.destroy_card:is_suit('Hearts') then
-        card.ability.extra.stored_cards[#card.ability.extra.stored_cards + 1] = context.destroy_card
+        -- store enough info here to re-create the card later
+        card.ability.extra.stored_cards[#card.ability.extra.stored_cards + 1] = {
+          id = context.destroy_card:get_id(),
+          center = context.destroy_card.center,
+          edition = context.destroy_card.edition,
+          perma_bonus = context.destroy_card.ability.perma_bonus or 0
+        }
+        -- animate oneself
+        -- card:juice_up()
+        G.E_MANAGER:add_event(Event({
+          func = function()
+            card:juice_up()
+            return true
+          end
+        }))
         return {
           message = "Arisen!",
           remove = true
@@ -87,5 +101,3 @@ end,
     end
   end
 }
-
--- 1EIM3G3H (shop 1 first joker pack)
